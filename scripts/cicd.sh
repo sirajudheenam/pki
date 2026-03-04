30c51767981b71ccc367cd082090f8b51089ab1c
Sirajudheen Mohamed Ali <sirajudheen.mohamed.ali@sap.com>
2026-03-04 10:21:17 +0100

    add experiment7

---
+++ scripts/cicd.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+echo "CURRENT : $pwd"
+echo 
+ls scripts/*
+ls /home/runner/work/pki/pki/*
+ls /home/runner/work/pki/pki/pki-demo/*
+ls /home/runner/work/pki/pki/demo-pki/localhost/*
+ls /home/runner/work/pki/pki/demo-pki/localhost/server/*
+ls /home/runner/work/pki/pki/demo-pki/localhost/root/*
+mkdir -p $(pwd)/pki-go/certs/localhost/server
+mkdir -p $(pwd)/pki-go/certs/localhost/client
+cp $(pwd)/demo-pki/localhost/client/* $(pwd)/pki-go/certs/localhost/client/
+cp $(pwd)/demo-pki/localhost/server/* $(pwd)/pki-go/certs/localhost/server/
+cp $(pwd)/demo-pki/localhost/root/* $(pwd)/pki-go/certs/localhost/server/
+ls /home/runner/work/pki/pki/*
+ls /home/runner/work/pki/pki/pki-demo/*
+ls /home/runner/work/pki/pki/pki-demo/certs/localhost/*
+ls /home/runner/work/pki/pki/pki-demo/certs/localhost/server/*
+ls /home/runner/work/pki/pki/pki-demo/certs/localhost/client/*
